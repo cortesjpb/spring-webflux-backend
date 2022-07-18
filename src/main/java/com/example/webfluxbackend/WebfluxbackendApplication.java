f4585ca6f2aa6a2fe3a1f1a30acde4b2d98ee5ad
Benjamin Cortes <benjamincortes@MacBook-Pro-de-Benjamin.local>
2022-07-18 20:15:19 -0300

    First Commit

---
+++ src/main/java/com/example/webfluxbackend/WebfluxbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfluxbackend/WebfluxbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
